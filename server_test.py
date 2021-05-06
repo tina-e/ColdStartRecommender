cd43aea5e739d8bd5972b8d3248e920f9dcc5828
Martina Emmert <emmert.tin1306@gmail.com>
2021-05-06 16:56:22 +0200

    implemented calculation of pseudo-ratings

--- server_test.py
+++ server_test.py
@@ -1,6 +1,25 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+#TODO
+def get_similar_users(cursor, k, category):
+    #cursor.execute(query)
+    return ["GERE", "tizian"]
+
+
+def get_pseudo_ratings(cursor, similar_pref_users):
+    target_values = ' OR '.join(f"link_name='{elem}'" for elem in similar_pref_users)
+    query = f"SELECT id, rating FROM kochbar_recipes_ratings WHERE {target_values}"
+    cursor.execute(query)
+
+    pseudo_ratings = {}
+    for elem in cursor:
+        pseudo_ratings.setdefault(elem[0], []).append(int(elem[1]))
+    pseudo_ratings = [(key, sum(values) / len(values)) for key, values in pseudo_ratings.items()]
+
+    return pseudo_ratings
+
+
 try:
       cnx = mysql.connector.connect(user='readonly', password='RoPlCa_readonly', host='132.199.143.90', port='8306',database='kochbar')
 except mysql.connector.Error as err:
@@ -10,12 +29,22 @@ except mysql.connector.Error as err:
         print("Database does not exist")
     else:
         print(err)
+
 else:
     cursor = cnx.cursor()
 
-    query = ("SELECT * FROM kochbar_recipes ORDER BY title LIMIT 10")
+    k = 5
+    category = "veggie_overlap"
+    similar_pref_users = get_similar_users(cursor, k, category)
+    pseudo_ratings = get_pseudo_ratings(cursor, similar_pref_users)
 
-    cursor.execute(query)
-    for element in cursor:
-        print(element)
     cnx.close()
+
+
+
+    #cursor = cnx.cursor()
+    #query = ("SELECT * FROM kochbar_recipes ORDER BY title LIMIT 10")
+    #cursor.execute(query)
+    #for element in cursor:
+    #    print(element)
+    #cnx.close()
