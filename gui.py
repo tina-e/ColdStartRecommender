ecf856737feeabd634958520245b5d605df27574
Martina Emmert <emmert.tin1306@gmail.com>
2021-05-20 17:43:59 +0200

    fixed price and difficulty

--- gui.py
+++ gui.py
@@ -81,10 +81,10 @@ class RecommenderInterface(QMainWindow):
             if self.input_username.text() in self.system.user_list:
                 self.label_error.setText("Username belegt.")
                 return False
-            if self.input_budget.text() == 'kleines Budget': budget = 1
-            elif self.input_budget.text() == 'mittleres Budget': budget = 3
-            elif self.input_budget.text() == 'höheres Budget': budget = 5
-            self.new_user = User(self.input_username.text(), self.input_level.text(), budget)
+            if self.input_budget.currentText() == 'kleines Budget': budget = 1
+            elif self.input_budget.currentText() == 'mittleres Budget': budget = 3
+            elif self.input_budget.currentText() == 'höheres Budget': budget = 5
+            self.new_user = User(self.input_username.text(), self.input_level.currentText(), budget)
 
         # type selection pages
         elif 'category' in stack_name:
